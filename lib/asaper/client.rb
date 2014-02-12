427777722c325eb6992cc44e382db2aef1b99a10
José Andrés Álvarez <alvarezloaiciga@gmail.com>
2014-02-12 16:40:19 -0600

    Refactor

--- lib/asaper/client.rb
+++ lib/asaper/client.rb
@@ -10,23 +10,25 @@ module Asaper
     end
 
     def logo_url(url)
-      @hash[:options] ||= {}
-      @hash[:options] = { logo_url: url }
+      new_attribute(:options, logo_url: url)
     end
 
     def title(title)
-      @hash[:message] ||= {}
-      @hash[:message].merge!(title: title)
+      new_attribute(:message, title: title)
     end
 
     def subtitle(subtitle)
-      @hash[:message] ||= {}
-      @hash[:message].merge!(subtitle: subtitle)
+      new_attribute(:message, subtitle: subtitle)
     end
 
     def content(content)
-      @hash[:message] ||= {}
-      @hash[:message].merge!(content: content)
+      new_attribute(:message, content: content)
+    end
+
+    private
+    def new_attribute(attribute, value)
+      @hash[attribute] ||= {}
+      @hash[attribute].merge!(value)
     end
   end
 end
